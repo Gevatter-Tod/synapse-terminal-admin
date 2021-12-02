c01af5db3acff15100f00162b13857680bf4e5a1
Gevatter-Tod <>
2021-12-02 12:15:09 +0100

    alpha 3 - user query basic implemented

--- synapse-admin.sh
+++ synapse-admin.sh
@@ -5,9 +5,9 @@ function generate_config {
     echo "No config file was found. Starting configuration (press Ctrl-C for cancle)"
     read -e -p "Sever Adress (URL): " SERVER_URL
     read -e -p "Admin Token: " ADMINTOKEN
-    echo $SERVER_URL and $ADMINTOKEN
-    echo "server: $SERVER_URL" > synapse-admin.conf
-    echo "admintoke: $ADMINTOKEN" >> synapse-admin.conf
+    echo "server: $SERVER_URL;" > synapse-admin.conf
+    echo "admintoken: $ADMINTOKEN;" >> synapse-admin.conf
+    echo "config generated"
 
 }
 
@@ -16,9 +16,9 @@ function read_config {
 while read y
 do
 if [[ "$y" == *"server"* ]]
-then echo "server is $y"
+then SERVER=$(awk '{ sub(/.*server: /, ""); sub(/;.*/, ""); print }' <<< "$y")
 elif [[ "$y" == *"admintoken"* ]]
-then echo "admintoken $y"
+then ADMINTOKEN=$(awk '{ sub(/.*admintoken: /, ""); sub(/;.*/, ""); print  }' <<< "$y")
 else echo "no server found: $y"
 fi
 done < synapse-admin.conf
@@ -42,14 +42,16 @@ function help {
 function users {
 
     echo "Query usernames..."
-    curl --header "Authorization: Bearer syt_cm9zZQ_gRgBVBbJZvGEiXleclGM_3SVQaX" "https://m.rosanelli.at/_synapse/admin/v2/users?from=0&limit=10&guests=false" | jq
+    curl --header "Authorization: Bearer $ADMINTOKEN" "https://$SERVER/_synapse/admin/v2/users?from=0&limit=10&guests=false" | jq
 }
 
 
 if test -f synapse-admin.conf
 then
+# Reading config
     echo "synapse-admin.conf found"
     read_config
+#Executing command
     if [ -z "$1" ]  || [ "$1" = "help" ]
     then help
     elif [ "$1" = "users" ]
